bc98074c031a1061277d9bc8dcccba30271c04a4
Afnan <wanafnanharizwz@gmail.com>
2021-01-07 22:08:54 +0800

    update gitignore

--- src/components/HomeAboutContact/Home.js
+++ src/components/HomeAboutContact/Home.js
@@ -29,15 +29,9 @@ export default function Home({motion=false,pageTransition,...props}) {
     return (
       <div>
         <h1>Home transition</h1>
-        <p>
-          Mollit eu fugiat do laboris ex anim consequat reprehenderit eu culpa
-          enim qui. Aliquip nulla aliquip nulla est culpa magna laborum sunt velit
-          eu excepteur aliqua culpa do. Sint cillum proident aliqua nostrud fugiat
-          occaecat magna veniam laborum ad nostrud. Officia aliqua sunt commodo
-          esse reprehenderit elit deserunt labore cupidatat quis sint id. Sit nisi
-          consequat ut Lorem id pariatur incididunt anim occaecat cupidatat
-          commodo est sit.
-        </p>
+          <div classname="home-div" >
+              
+          </div>
       </div>
     );
 
